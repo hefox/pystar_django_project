e10e3be4cfa143720d2ad9ca624a8ab9a6401af4
Fox <fox@goingon.com>
2011-04-30 11:08:42 -0700

    Set up the URLs for polls

--- urls.py
+++ urls.py
@@ -5,9 +5,13 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^polls/$', 'polls.views.index'),
+    (r'^polls/(\d+)/$', 'polls.views.detail'),
+    (r'^polls/(\d+)/results/$', 'polls.views.results'),
+    (r'^polls/(\d+)/vote/$', 'polls.views.vote'),
     # Examples:
-    # url(r'^$', 't.views.home', name='home'),
-    # url(r'^t/', include('t.foo.urls')),
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^myproject/', include('myproject.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,3 +19,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
