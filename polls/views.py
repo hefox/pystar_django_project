f162a6843aa4abe59c6c2e04bd0a74e48a7d406c
Fox <fox@goingon.com>
2011-04-30 12:39:42 -0700

    Making shit pretty

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,24 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from polls.models import Poll
+from django.http import Http404
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render_to_response('polls/index.html', context)
+
 # recall or note that %s means, "subsitute in a string"
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % (poll_id,))
+    try:
+        p = Poll.objects.get(id=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % (poll_id,))
